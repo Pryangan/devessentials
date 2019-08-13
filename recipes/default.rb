85e6c44920adbe642e1533fecbeb0db2f42798cd
Pryangan Chowdhury <pryangan.chowdhury@gmail.com>
2019-08-13 20:44:19 +0530

    adds a line of text to default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,3 +4,15 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 # First Changes by pryangan
+
+package 'httpd'
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello, world!'</h1>
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+Hello.. this is a git push test
